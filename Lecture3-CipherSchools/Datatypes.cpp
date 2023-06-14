6e3a24b539bd04ce03d371602ceedd7656b0b2f9
Akshiita <akshita.12002351@lpu.in>
2023-06-14 18:39:43 +0530

    Added Datatypes.cpp file

---
+++ Lecture3-CipherSchools/Datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ //declare 2 variables and then assign them values 3,5 then add them 
+int a , b ;
+a=3;
+b=5;
+int c = a+b;
+cout<<"Value of c is "<<c;
+return 0;
+}
